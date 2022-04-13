31f8ef080504b907051e838b3ab3b588d7da8a04
Gusttavo210 <102116359+Gusttavo210@users.noreply.github.com>
2022-04-13 14:13:42 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php 
+
+    # https://www.php.net/manual/en/function.date-default-timezone-set
+    # https://www.php.net/manual/pt_BR/function.spl-autoload-register.php
+
+    date_default_timezone_set('Campo_de_Grande/Rio_de_Janeiro');
+
+    # incluir as classes do diretório php/model
+    spl_autoload_register(function($file_name){
+
+        $fileName = "php" . DIRECTORY_SEPARATOR . "model" . DIRECTORY_SEPARATOR . "{$file_name}.php";
+
+        if(file_exists($fileName)) {
+            require_once($fileName);
+        }
+    });
+
+    # incluir as classes do diretório php/repository
+    spl_autoload_register(function($file_name){
+
+        $fileName = "php" . DIRECTORY_SEPARATOR . "repository" . DIRECTORY_SEPARATOR . "{$file_name}.php";
+
+        if(file_exists($fileName)) {
+            require_once($fileName);
+        }
+    });
